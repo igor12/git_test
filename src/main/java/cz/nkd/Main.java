6ef608ad5015b17ed92278779935210eda170ccc
john <michal.nikodim@gmail.com>
2014-04-23 09:06:24 +0200

    first commit

---
+++ src/main/java/cz/nkd/Main.java
@@ -0,0 +1,12 @@
+package cz.nkd;
+
+/**
+ * @author Michal NkD Nikodim (michal.nikodim@topmonks.com)
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
