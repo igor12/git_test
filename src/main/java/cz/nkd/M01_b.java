4811b607eeffea891ff582ab12143a9c26b60302
john <john@john.john>
2014-04-23 11:54:50 +0200

    m01

---
+++ src/main/java/cz/nkd/M01_b.java
@@ -0,0 +1,8 @@
+package cz.nkd;
+
+/**
+ * @author Michal NkD Nikodim (michal.nikodim@topmonks.com)
+ */
+public class M01_b {
+
+}
--- src/main/java/cz/nkd/Main.java
+++ src/main/java/cz/nkd/Main.java
@@ -6,7 +6,7 @@ package cz.nkd;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world - M01");
     }
 
 }
